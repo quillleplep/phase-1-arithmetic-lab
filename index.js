2e31170cdaa779648d760e4abbde70d1d7a18fae
quill_nichols <leplep00100@gmail.com>
2023-12-11 23:03:30 -0500

    MATHS

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
+//1)
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+//2)
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+//3)
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+//4)
+let max = Math.max(10, 20, 5);
 // Write your code here
